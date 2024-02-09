d0597372d8b3caee5c1fbab5437416deb6ef1fe7
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-02-09 20:50:28 -0300

    update_v3

---
+++ src/assets/js/lgs.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const rmInput = document.getElementById("rm");
+
+    rmInput.addEventListener("input", function () {
+        // Remove qualquer caractere não numérico do valor do campo RM
+        const rmValue = this.value.replace(/[^0-9]/g, "");
+
+        // Limita o valor a 5 dígitos
+        const maxLength = 5;
+        const truncatedValue = rmValue.slice(0, maxLength);
+
+        // Atualiza o valor do campo RM
+        this.value = truncatedValue;
+    });
+})
