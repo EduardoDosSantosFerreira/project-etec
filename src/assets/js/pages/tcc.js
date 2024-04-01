3b1e5a2469707ca3043a75a809a6de2a1f0d071b
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-03-31 23:32:47 -0300

    update_v1.0.3

---
+++ src/assets/js/pages/tcc.js
@@ -0,0 +1,173 @@
+// Função para adicionar o HTML ao DOM
+function adicionarLoginAoDOM() {
+  var div = document.createElement("div");
+  div.innerHTML = `
+  <main>
+    <!-- introdução aos cursos técnicos -->
+    <div class="container-wrapper" data-aos="fade-left">
+        <h2>
+            <p id="char02">Guia de trabalho de conclusão de curso
+        </h2>
+    </div>
+    <div class="spacex"></div>
+    <section id="seck02">
+    </section>
+    <div class="container">
+        <br>
+        <section class="home-intro" data-aos="fade-right">
+            <div class="home-intro__wrapper">
+                <aside class="embed">
+                    <a href="../pages/dds.html"><img id="radius-img" class="img-fluid" width="560" height="315"
+                            src="../assets/img/tcc/img01.webp" alt="operação em computador e celular"
+                            title="Manual de normas para TCC"></a>
+                </aside>
+                <div>
+                    <h2 class="home-intro__title">
+                        Conheça o
+                        <a href="../pages/dds.html">
+                            <strong style="color: #00758f;">Manual de normas </strong></a>
+                    </h2>
+                    <p class="home-intro__text">
+                        As diretrizes aqui contidas têm por objetivo organizar e padronizar o desenvolvimento, a
+                        construção,
+                        avaliação, validação e apresentação dostrabalhos de conclusão de curso (TCC’s) dos Cursos de
+                        Ensino Médio
+                        Integrado ao Técnico nas Habilitações de administração, Infomática para Internet e Meio Ambiente
+                        e também
+                        dos Cursos Modulares de Habilitação Técnica de administração, Desenvolvimento de Sistemas e
+                        Farmácia
+                        da Etec de Itanhaém, criando uma comunicação e orientação interna que atenda a alunos,
+                        professores e
+                        avaliadores.-
+                        O documento está baseado nas recomendações da Associação Brasileira de Normas Técnicas (ABNT)
+                        e em conformidade com o Manual para a elaboração do Trabalho de Conclusão de Curso das Escolas
+                        Técnicas
+                        do Centro Paula Souza.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <br>
+        <br>
+        <section class="home-intro" data-aos="fade-right">
+            <div class="home-intro__wrapper">
+                <aside class="embed">
+                    <a href="../pages/adm.html"><img id="radius-img" width="560" class="img-fluid" height="315"
+                            src="../assets/img/tcc/img02.jpeg" alt="gráficos e diagramas"
+                            title="Orientação para TCC"></a>
+                </aside>
+                <div>
+                    <h2 class="home-intro__title">
+                        Conheça as
+                        <a href="../pages/adm.html">
+                            <strong style="color: #3947b0;">Orientações básicas para elaboração de TCC</strong></a>
+                    </h2>
+                    <p class="home-intro__text">
+                        Para criar um TCC eficaz, escolha um tema relevante, formule uma pergunta de pesquisa, faça
+                        revisão bibliográfica, defina objetivos e metodologia, colete e analise dados (se necessário),
+                        organize o trabalho em seções, escreva com clareza, cite corretamente as fontes, cumpra os
+                        prazos e prepare-se para a apresentação, buscando orientação de um professor.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <br>
+        <br>
+        <section class="home-intro" data-aos="fade-right">
+            <div class="home-intro__wrapper">
+                <aside class="embed">
+                    <a href="fma.html"><img id="radius-img" width="560" class="img-fluid" height="315"
+                            src="../assets/img/tcc/img03.webp" alt="farmacêuticos construindo remédio"
+                            title="Termos TCC"></a>
+                </aside>
+                <div>
+                    <h2 class="home-intro__title">
+                        Conheça o
+                        <a href="../pages/fma.html">
+                            <strong style="color: #a71d2a;">Termos para TCC</strong></a>
+                    </h2>
+                    <p class="home-intro__text">
+                        As normas de um Trabalho de Conclusão de Curso (TCC) são diretrizes essenciais que garantem a
+                        organização e qualidade da pesquisa acadêmica. Elas incluem regras de formatação, estruturação,
+                        citações, referências, ética e apresentação. A conformidade com essas normas varia de acordo com
+                        a instituição e o estilo de formatação adotado (APA, ABNT, Chicago, etc.). É crucial que os
+                        estudantes consultem o manual de normas da instituição e busquem orientação do orientador para
+                        seguir corretamente todas as diretrizes, a fim de assegurar a credibilidade e qualidade do TCC.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+    <br>
+    <br>
+    <section class="home-intro" data-aos="fade-right">
+        <div class="home-intro__wrapper">
+            <aside class="embed">
+                <a href="fma.html"><img id="radius-img" width="560" class="img-fluid" height="315"
+                        src="https://images.pexels.com/photos/261909/pexels-photo-261909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt="farmacêuticos construindo remédio" title="Livro TCC"></a>
+            </aside>
+            <div>
+                <h2 class="home-intro__title">
+                    Conheça o
+                    <a href="fma.html">
+                        <strong style="color: #a71d2a;">Livro TCC</strong></a>
+                </h2>
+                <p class="home-intro__text">
+                    O Projeto Manual Técnico Centro Paula Souza – Coleção Técnica Interativa oferece aos alunos da
+                    instituição conteúdo relevante à formação técnica, à
+                    educação e à cultura nacional, sendo também sua finalidade a preservação e a divulgação desse
+                    conteúdo, respeitados os direitos de terceiros.
+                    O material apresentado é de autoria de professores do Centro Paula Souza e resulta de experiência na
+                    docência e da pesquisa em fontes como livros,
+                    artigos, jornais, internet, bancos de dados, entre outras, com a devida autorização dos detentores
+                    dos direitos desses materiais ou contando com a permissibilidade legal, apresentando, sempre que
+                    possível, a indicação da autoria/crédito e/ou reserva de direitos de cada um deles.
+                    Todas as obras e imagens expostas nesse trabalho são protegidas pela legislação brasileira e não
+                    podem ser reproduzidas ou utilizadas por terceiros, por
+                    qualquer meio ou processo, sem expressa autorização de seus titulares.
+                </p>
+            </div>
+        </div>
+    </section>
+    </div>
+    <br>
+    <br>
+    <section class="home-intro" data-aos="fade-right">
+        <div class="home-intro__wrapper">
+            <aside class="embed">
+                <a href="fma.html"><img id="radius-img" width="560" class="img-fluid" height="315"
+                        src="https://www.3gsolucoes.com/wp-content/uploads/2019/01/suporte-l-capa.png"
+                        alt="farmacêuticos construindo remédio" title="Livro TCC"></a>
+            </aside>
+            <div>
+                <h2 class="home-intro__title">
+                    Conheça o
+                    <a href="fma.html">
+                        <strong style="color: #a71d2a;"> Banner para TCC</strong></a>
+                </h2>
+                <p class="home-intro__text">
+                    Aqui está o arquivo (Corel Draw) ao qual será utilizado para desenvolver e confeccionar o banner
+                    para a apresentação e feira do TCC.A criação de um banner eficaz para a apresentação de um TCC é
+                    fundamental para transmitir informações de forma clara e cativante. É importante seguir uma
+                    estrutura lógica que reflita a organização do TCC, usar um título impactante, incluir elementos
+                    visuais como gráficos e tabelas, manter o texto conciso, destacar os principais resultados e
+                    conclusões, escolher uma formatação atraente e pedir feedback. Praticar a apresentação é essencial
+                    para garantir que o banner seja eficaz na comunicação das descobertas e conquistas do trabalho.
+                </p>
+            </div>
+        </div>
+    </section>
+    </div>
+    <br>
+    <br>
+    <!-- fim da introdução aos cursos técnicos -->
+
+    <!-- background final class  -->
+</main>
+    `;
+
+  document.body.appendChild(div);
+}
+
+adicionarLoginAoDOM();
